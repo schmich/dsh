edd6533325602401a546d51df09e4792f34049a1
Chris Schmich <schmch@gmail.com>
2016-11-09 07:08:36 -0600

    Rename build script to build.rb since it works cross-platform.

---
+++ build.rb
@@ -0,0 +1,11 @@
+#!env ruby
+
+def run(command)
+  puts command
+  system command
+end
+
+version = `git tag`.lines.last.strip
+commit = `git rev-parse HEAD`.strip
+
+run("go build -ldflags \"-w -s -X main.version=#{version} -X main.commit=#{commit}\"")
